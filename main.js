7e5ccaa529a43b9bd1f6bfa2686daa9fe1e7e6e1
aZeDen <lucasgosselin3101@gmail.com>
2025-06-02 11:20:28 +0200

    Ajout des questions sous la forme d'un tableau d'objet

---
+++ main.js
@@ -0,0 +1,58 @@
+const questions = [
+  {
+    question: "Quel était l'un des instruments de torture les plus utilisés pendant l'Inquisition espagnole ?",
+    reponseA: "Le chevalet",
+    reponseB: "La guillotine",
+    reponseC: "La chaise électrique",
+    reponseD: "Le bûcher",
+    bonneReponse: "reponseA"
+  },
+  {
+    question: "Quelle civilisation antique utilisait le supplice du taureau d'airain ?",
+    reponseA: "Les Romains",
+    reponseB: "Les Grecs",
+    reponseC: "Les Perses",
+    reponseD: "Les Égyptiens",
+    bonneReponse: "reponseB"
+  },
+  {
+    question: "Dans quelle prison française célèbre la torture a-t-elle été systématiquement utilisée pendant la guerre d'Algérie ?",
+    reponseA: "La Santé",
+    reponseB: "Montluc",
+    reponseC: "La prison de Serkadji",
+    reponseD: "La villa Susini",
+    bonneReponse: "reponseD"
+  },
+  {
+    question: "Quel texte international interdit explicitement la torture en toutes circonstances ?",
+    reponseA: "La Déclaration universelle des droits de l'homme",
+    reponseB: "Le Pacte international relatif aux droits civils et politiques",
+    reponseC: "La Convention contre la torture de l'ONU",
+    reponseD: "La Convention de Genève",
+    bonneReponse: "reponseC"
+  },
+  {
+    question: "Quel organe de l'ONU est chargé de surveiller l'application de la Convention contre la torture ?",
+    reponseA: "Le Conseil de sécurité",
+    reponseB: "Le Comité contre la torture",
+    reponseC: "La Cour pénale internationale",
+    reponseD: "Le Haut-Commissariat aux droits de l'homme",
+    bonneReponse: "reponseB"
+  },
+  {
+    question: "Quel pays a été accusé d'avoir pratiqué des “interrogatoires renforcés” assimilables à de la torture après les attentats du 11 septembre 2001 ?",
+    reponseA: "Russie",
+    reponseB: "Chine",
+    reponseC: "États-Unis",
+    reponseD: "Arabie saoudite",
+    bonneReponse: "reponseC"
+  },
+  {
+    question: "Dans combien de pays environ la torture est-elle encore pratiquée selon Amnesty International (estimation récente) ?",
+    reponseA: "Moins de 10",
+    reponseB: "Environ 30",
+    reponseC: "Plus de 70",
+    reponseD: "Plus de 150",
+    bonneReponse: "reponseC"
+  }
+];
