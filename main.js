e37c116dac40911f1636fdbd713ebdba67aabc8c
aZeDen31 <lucasgosselin3101@gmail.com>
2025-06-03 19:28:34 +0200

    Ajout du score en bas de la page

--- main.js
+++ main.js
@@ -5,42 +5,57 @@ let currentQuestion;
 let pastQuestions = [];
 let cntJ1 = 0;
 let cntJ2 = 0;
+let cntGlobal = 0;
+let joueur1 = true;
 
 boutons.forEach(bouton => {
-    bouton.addEventListener("click", () => reponseQuestion(bouton));
+	bouton.addEventListener("click", () => reponseQuestion(bouton));
 });
 
-function pickRandomQuestion(){
-    let n = Math.floor(Math.random() * questions.length);
-    if(pastQuestions.includes(n)){
-      return pickRandomQuestion();
-    } else {
-      pastQuestions.push(n);
-      console.log(pastQuestions.length)
-      return questions[n];
-    }
+// Fonction qui choisit une question au hasard. Je cherche à savoir si n (le numéro de la question) est déjà apparu pour ne pas avoir de doublon au niveau des questions
+function pickRandomQuestion() {
+	let n = Math.floor(Math.random() * questions.length);
+	if (pastQuestions.includes(n)) {
+		return pickRandomQuestion();
+	} else {
+		pastQuestions.push(n);
+		console.log("Longueur : " + pastQuestions.length);
+		return questions[n];
+	}
 }
 
-export function randomQuestionDisplay(){
-    let selectedQuestion = pickRandomQuestion();
-    currentQuestion = selectedQuestion;
-    let valeurs = Object.values(selectedQuestion);
-    let cles = Object.keys(selectedQuestion);
-    let texte = document.getElementById("questionText");
-    texte.innerText = selectedQuestion["question"];
-    let i = 1;
-    boutons.forEach(bouton => {
-        bouton.innerText = valeurs[i];
-        bouton.classList.add(cles[i]);
-        i++;
-    });
+// La fonction met la question en haut de l'écran et ajoute les réponses sur les boutons
+export function randomQuestionDisplay() {
+	let selectedQuestion = pickRandomQuestion();
+	currentQuestion = selectedQuestion;
+	let valeurs = Object.values(selectedQuestion);
+	let cles = Object.keys(selectedQuestion);
+	let texte = document.getElementById("questionText");
+	texte.innerText = selectedQuestion["question"];
+	let i = 1;
+	boutons.forEach(bouton => {
+		bouton.innerText = valeurs[i];
+		bouton.classList.add(cles[i]);
+		i++;
+	});
 }
 
-function reponseQuestion(bouton){
-    let classes = bouton.classList;
-    if(classes.contains(currentQuestion.bonneReponse)){
-      randomQuestionDisplay();
-    }
+// Fonction simple qui vérifie si le bouton sur lequel on clique est la bonne réponse
+function reponseQuestion(bouton) {
+	let classes = bouton.classList;
+	let scoreJ1 = document.getElementById("scoreJ1");
+	let scoreJ2 = document.getElementById("scoreJ2");
+	if (classes.contains(currentQuestion.bonneReponse)) {
+		cntGlobal++;
+		joueur1 ? cntJ1++ : cntJ2++;
+		joueur1 ? scoreJ1.innerText = "Joueur 1 : " + cntJ1 : scoreJ2.innerText = "Joueur 2 : " + cntJ2 // Met à jour le texte du score en bas de la page
+		joueur1 = !joueur1
+		randomQuestionDisplay(); // Tire une nouvelle question.
+	} else {
+		cntGlobal++;
+		joueur1 = !joueur1
+		randomQuestionDisplay();
+	}
 }
 
 randomQuestionDisplay();
